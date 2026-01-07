5e8547167cca81191f8c2e575cabfd4608964861
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2026-01-07 15:10:48 +1300

    Adding basic Todo to index and adding git ignore.

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+<!DOCTYPE html>
 
+/* TODO: Drop down to select what day to solve
+This should list all the way up to 12b
+If there is no solution for this day yet that should come up
+*/
+
+/* TODO: Select the present input file, upload a file, or input a text input
+Check if the input file is present, show error if not
+*/
