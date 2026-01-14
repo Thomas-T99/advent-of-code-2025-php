c51f58708daeb3d8a324496c9f79d415752d81a0
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2026-01-14 23:54:58 +1300

    Added function outlines

--- includes/solutions/day_1.php
+++ includes/solutions/day_1.php
@@ -1,1 +1,14 @@
 <?php
+
+function process_input($input_file){
+    fopen($input_file, "r") or die("Unable to open file!");
+    fclose($input_file);
+}
+
+function part1($input){
+
+}
+
+function part2($input){
+
+}
