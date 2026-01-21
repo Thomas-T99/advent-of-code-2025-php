7947ffc797b5ae06b770679cf2f5207188f1ec06
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2026-01-21 14:52:47 +1300

    Day 1 part 2 complete

--- includes/solutions/day_1.php
+++ includes/solutions/day_1.php
@@ -31,5 +31,42 @@ function part1($input){
 }
 
 function part2($input){
-    return "Not completed";
+    $zero_count = 0;
+    $dial_count = 50;
+    foreach($input as $line){
+        $direction = $line[0];
+        $count = $line[1];
+
+        // Count and remove all full rotations in a request
+        $full_rotations = intdiv($count, 100);
+        $zero_count += $full_rotations;
+        $count = $count % 100;
+
+
+
+        if ($dial_count == 0) {
+            $starting_zero = true;
+        } else {
+            $starting_zero = false;
+        }
+
+        if ($direction == "L") {
+            $count = -$count;
+        }
+        $dial_count += $count;
+
+        if ($dial_count == 0) {
+            $zero_count++;
+        } elseif ($dial_count < 0) {
+            if (!$starting_zero) { // otherwise we would count the zero twice, at the end of the last rotation (below) and during this one
+                $zero_count++;
+            }
+            $dial_count = $dial_count + 100;
+        } elseif ($dial_count >= 100) {
+            $zero_count++;
+            $dial_count = $dial_count - 100;
+        }
+    }
+
+    return $zero_count;
 }
