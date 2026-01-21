73436bf1901aff9f62fe390f90b3add190c82f95
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2026-01-21 18:05:43 +1300

    Completed day 2 part 1. Solution feels naive but still calculates fast.

--- includes/solutions/day_1.php
+++ includes/solutions/day_1.php
@@ -1,50 +1,48 @@
 <?php
 
-function process_input($input_file){
-    $lines = file($input_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+function process_input($inputFile){
+    $lines = file($inputFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     $input = array();
     foreach($lines as $line){
-        $inputarray = array(substr($line, 0,1), substr($line, 1));
-        array_push($input, $inputarray);
+        $inputArray = array(substr($line, 0,1), substr($line, 1));
+        array_push($input, $inputArray);
     }
     return $input;
 }
 
 function part1($input){
-    $zero_count = 0;
-    $dial_count = 50;
+    $zeroCount = 0;
+    $dialCount = 50;
     foreach($input as $line){
         $direction = $line[0];
         $count = $line[1];
         if ($direction == "L") {
             $count = -$count;
         }
-        $dial_count += $count;
-        $dial_count = $dial_count % 100;
-        $dial_count = $dial_count + 100;
-        $dial_count = $dial_count % 100;
-        if ($dial_count == 0) {
-            $zero_count++;
+        $dialCount += $count;
+        $dialCount = $dialCount % 100;
+        $dialCount = $dialCount + 100;
+        $dialCount = $dialCount % 100;
+        if ($dialCount == 0) {
+            $zeroCount++;
         }
     }
-    return $zero_count;
+    return $zeroCount;
 }
 
 function part2($input){
-    $zero_count = 0;
-    $dial_count = 50;
+    $zeroCount = 0;
+    $dialCount = 50;
     foreach($input as $line){
         $direction = $line[0];
         $count = $line[1];
 
         // Count and remove all full rotations in a request
         $full_rotations = intdiv($count, 100);
-        $zero_count += $full_rotations;
+        $zeroCount += $full_rotations;
         $count = $count % 100;
 
-
-
-        if ($dial_count == 0) {
+        if ($dialCount == 0) {
             $starting_zero = true;
         } else {
             $starting_zero = false;
@@ -53,20 +51,20 @@ function part2($input){
         if ($direction == "L") {
             $count = -$count;
         }
-        $dial_count += $count;
+        $dialCount += $count;
 
-        if ($dial_count == 0) {
-            $zero_count++;
-        } elseif ($dial_count < 0) {
+        if ($dialCount == 0) {
+            $zeroCount++;
+        } elseif ($dialCount < 0) {
             if (!$starting_zero) { // otherwise we would count the zero twice, at the end of the last rotation (below) and during this one
-                $zero_count++;
+                $zeroCount++;
             }
-            $dial_count = $dial_count + 100;
-        } elseif ($dial_count >= 100) {
-            $zero_count++;
-            $dial_count = $dial_count - 100;
+            $dialCount = $dialCount + 100;
+        } elseif ($dialCount >= 100) {
+            $zeroCount++;
+            $dialCount = $dialCount - 100;
         }
     }
 
-    return $zero_count;
+    return $zeroCount;
 }
--- includes/solutions/day_2.php
+++ includes/solutions/day_2.php
@@ -1,1 +1,44 @@
 <?php
+
+function process_input($inputFile){
+    $fileContents = file_get_contents($inputFile);
+    $ranges = explode(",", $fileContents);
+    $input = array();
+    foreach($ranges as $range){
+        $rangeArray = explode("-", $range);
+        array_push($input, $rangeArray);
+    }
+    return $input;
+}
+
+function part1($input){
+    $invalidSum = 0;
+    foreach($input as $range){
+        $min = $range[0];
+        $max = $range[1];
+        $minLength = strlen($min);
+        $maxLength = strlen($max);
+
+        if($minLength == $maxLength && $minLength % 2 != 0) {
+            continue;
+        }
+
+        for($int = $min; $int < $max; $int++){
+            $str = (string)$int;
+            $strLength = strlen($str);
+            if($strLength % 2 != 0){
+                continue;
+            }
+            $firstHalf = substr($str, 0, $strLength / 2);
+            $lastHalf = substr($str, $strLength / 2);
+            if($firstHalf == $lastHalf){
+                $invalidSum += $int;
+            }
+        }
+    }
+    return $invalidSum;
+}
+
+function part2($input){
+    return "Not yet completed";
+}
